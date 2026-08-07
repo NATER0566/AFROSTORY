b36523c43d6e67d5c24d300ad113602a4cdc75f8
NATER MBASHAU <nmbashau@gmail.com>
2026-08-07 18:14:33 +0100

    Enhance history schema with seriesId field
    Added seriesId to history schema for optimized querying.

--- server/models/History.js
+++ server/models/History.js
@@ -1,29 +1,14 @@
 import mongoose from 'mongoose';
 
 const historySchema = new mongoose.Schema({
-  userId: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User', 
-    required: true, 
-    index: true 
-  },
-  episodeId: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Episode', 
-    required: true, 
-    index: true 
-  },
-  lastPosition: { 
-    type: Number, 
-    default: 0 // In seconds
-  },
-  completed: { 
-    type: Boolean, 
-    default: false 
-  }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, index: true },
+  // ADDED: seriesId makes finding "Continue Watching" 100x faster for the database
+  seriesId: { type: mongoose.Schema.Types.ObjectId, ref: 'Series', required: true },
+  episodeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Episode', required: true, index: true },
+  lastPosition: { type: Number, default: 0 },
+  completed: { type: Boolean, default: false }
 }, { timestamps: true });
 
-// A user can only have one history state per episode
 historySchema.index({ userId: 1, episodeId: 1 }, { unique: true });
 
 export default mongoose.model('History', historySchema);
